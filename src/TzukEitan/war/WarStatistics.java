ae279ea5b125c22fa737baa34f37220bd770976a
amirben <amir.ben@gmail.com>
2014-08-17 23:07:22 +0300

    war statistics

--- src/TzukEitan/war/WarStatistics.java
+++ src/TzukEitan/war/WarStatistics.java
@@ -1,5 +1,57 @@
 package TzukEitan.war;
-//check
-public class WarStatistics {
 
+public class WarStatistics {
+	private long numOfLaunchMissiles;
+	private long numOfInterceptMissiles;
+	private long numOfHitTargetMissiles;
+	private long numOfLaunchersDestroyed;
+	private long totalDamage;
+	
+	public WarStatistics(){
+		numOfLaunchMissiles = 0;
+		numOfInterceptMissiles = 0;
+		numOfHitTargetMissiles = 0;
+		numOfLaunchersDestroyed = 0;
+		totalDamage = 0;
+	}
+	
+	public synchronized void increaseNumOfLaunchMissiles(){
+		numOfLaunchMissiles++;
+	}
+	
+	public synchronized void increaseNumOfInterceptMissiles(){
+		numOfInterceptMissiles++;
+	}
+	
+	public synchronized void increaseNumOfHitTargetMissiles(){
+		numOfHitTargetMissiles++;
+	}
+	
+	public synchronized void increaseNumOfLauncherDestroyed(){
+		numOfLaunchersDestroyed++;
+	}
+	
+	public synchronized void increaseTotalDamage(int damage){
+		totalDamage += damage;
+	}
+	
+	public long getNumOfLaunchMissiles(){
+		return numOfLaunchMissiles;
+	}
+	
+	public long getNumOfInterceptMissiles(){
+		return numOfInterceptMissiles;
+	}
+	
+	public long getNumOfHitTargetMissiles(){
+		return numOfHitTargetMissiles;
+	}
+	
+	public long getNumOfLaunchersDestroyed(){
+		return numOfLaunchersDestroyed;
+	}
+	
+	public long getTotalDamage(){
+		return totalDamage;
+	}
 }
